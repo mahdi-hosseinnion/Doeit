c50537981cc5c6b73263d231b6c05e028dec64fd
mahdi hosseinnion <mahdimahdismh@gmail.com>
2024-11-27 23:54:26 +0330

    Add check box to task item

--- app/src/main/java/com/ssmmhh/doeit/tasks/TasksScreen.kt
+++ app/src/main/java/com/ssmmhh/doeit/tasks/TasksScreen.kt
@@ -1,14 +1,23 @@
 package com.ssmmhh.doeit.tasks
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ssmmhh.doeit.data.Task
+import com.ssmmhh.doeit.ui.theme.DoeitTheme
 
 
 @Composable
@@ -29,7 +38,7 @@ fun TaskScreen(
         modifier = modifier
     ) {
         items(todayTasks) {
-            TaskItem(it)
+            TaskItem(it, {})
         }
     }
 }
@@ -37,7 +46,35 @@ fun TaskScreen(
 @Composable
 fun TaskItem(
     task: Task,
+    onToggleCompleted: (Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Text(task.title)
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.padding(horizontal = 16.dp)
+    ) {
+        Checkbox(
+            checked = task.isCompleted,
+            onCheckedChange = onToggleCompleted
+        )
+        Text(
+            task.title
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskItemPreview() {
+    DoeitTheme {
+        Surface {
+            TaskItem(
+                Task(
+                    title = "Wash the dishes",
+                    isCompleted = true
+                ),
+                {}
+            )
+        }
+    }
 }
