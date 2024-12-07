fb1b76add98cf77d51f7015f9ea077cb2a1284a8
mahdi hosseinnion <mahdimahdismh@gmail.com>
2024-12-08 00:13:09 +0330

    Add transition animation between compose screens

--- app/src/main/java/com/ssmmhh/doeit/NavHost.kt
+++ app/src/main/java/com/ssmmhh/doeit/NavHost.kt
@@ -1,5 +1,12 @@
 package com.ssmmhh.doeit
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -26,8 +33,34 @@ fun DoeitNavHost() {
     val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = Tasks) {
-        composable<Tasks> { TasksScreen(navController) }
-        composable<TaskDetail> { TaskDetailScreen(navController = navController) }
+        val animationDuration = 300
+        composable<Tasks>(
+            enterTransition = {
+                fadeIn(animationSpec = tween(animationDuration, easing = LinearEasing))
+            },
+            exitTransition = {
+                fadeOut(animationSpec = tween(animationDuration, easing = LinearEasing))
+            }
+
+        ) { TasksScreen(navController) }
+        composable<TaskDetail>(
+            enterTransition = {
+                fadeIn(
+                    animationSpec = tween(animationDuration, easing = LinearEasing)
+                ) + slideIntoContainer(
+                    animationSpec = tween(animationDuration, easing = EaseIn),
+                    towards = AnimatedContentTransitionScope.SlideDirection.Start
+                )
+            },
+            exitTransition = {
+                fadeOut(
+                    animationSpec = tween(animationDuration, easing = LinearEasing)
+                ) + slideOutOfContainer(
+                    animationSpec = tween(animationDuration, easing = EaseOut),
+                    towards = AnimatedContentTransitionScope.SlideDirection.End
+                )
+            }
+        ) { TaskDetailScreen(navController = navController) }
         dialog<QuickAdd>(dialogProperties = QuickDialogDialogProperties) {
             QuickAddDialog(
                 navController = navController
